c9e0b428a14bf60efe39d5894fa321bbc79c6007
ypghp2zbvr-commits <251344143+ypghp2zbvr-commits@users.noreply.github.com>
2026-05-06 17:42:21 +0900

    yml修正とplaywright.config修正

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
+    webServer: {
+        command: 'npm run dev' , //テスト用の環境を呼び出す起動コマンド
+        url: 'http://localhost:3000',
+        reuseExistingServer: !process.env.CI,
+        stdout: 'ignore',
+        stderr: "pipe",
+    },
     use: {
         // 環境変数があればそれを使う、なければローカルを使う
         baseURL: process.env.BASE_URL || 'http://localhost:3000',
